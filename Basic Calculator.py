33b8aeaedf25a79089dd650ad41aaaa5d5e428a5
karansingh77-alt <darkop2929@gmail.com>
2026-07-02 15:08:31 +0530

    Add files via upload

---
+++ Basic Calculator.py
@@ -0,0 +1,6 @@
+num1 = int(input("Write first number: ") )
+num2 = int(input("Write second number: ") )
+print("addition = " , num1 + num2 )
+print("subtraction = " , num1 - num2 )
+print("multiplication = " , num1 * num2 )
+print("divison = " , num1 / num2 )
